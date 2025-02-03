06c9fecafe819f133d57965a886b5b006db545c1
henrique <henriquesevdacunha@gmail.com>
2025-02-03 11:59:33 -0400

    Adicionado o carrossel na aba home

--- controllerapp/controllerapp/views.py
+++ controllerapp/controllerapp/views.py
@@ -1,8 +1,10 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from Controle_ar.models import CarouselImage
 
 def homepage(request):
-    return render(request, "home.html")
+    carousel_images = CarouselImage.objects.filter(active=True)
+    return render(request, 'home.html', {'carousel_images': carousel_images})
 
 def about(request):
     return render(request, "about.html")
