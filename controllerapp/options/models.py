b89662f3840f9750029da7813820aa79e05e59a1
henrique <henriquesevdacunha@gmail.com>
2025-02-10 18:55:16 -0400

    Implementada a tela de materiais

--- controllerapp/options/models.py
+++ controllerapp/options/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Material(models.Model):
+    Nome_do_meterial = models.CharField(max_length=100)
+    imagem_do_material = models.ImageField(upload_to='materials/')
+    descrição_do_material = models.TextField()
+
+    def __str__(self):
+        return self.Nome_do_meterial
