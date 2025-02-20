34a8400846c3b851d1acd98ad4cd0cfb67a199f3
henrique <henriquesevdacunha@gmail.com>
2025-02-20 17:19:49 -0400

    adicionado a função do cartão de estudante

--- controllerapp/users/models.py
+++ controllerapp/users/models.py
@@ -1,25 +1,17 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
-from django.db import models
 from django.utils.translation import gettext_lazy as _
 
 from .managers import CustomUserManager
 
-# Create your models here.
-
 class CustomUser(AbstractUser):
-    """
-    Custom user model that fits IFMT's use case, adding a numerical
-    ID that serves as the USERNAME_FIELD.
-    """
-
     username = None
     first_name = models.CharField(_("first name"), max_length=150, blank=False)
     last_name = models.CharField(_("last name"), max_length=150, blank=False)
     email = models.EmailField(_("email address"), blank=False)
 
     id = models.CharField(_("matricula"), 
-        max_length=13 ,
+        max_length=13,
         unique=True, 
         primary_key=True,
         help_text=_(
@@ -37,9 +29,16 @@ class CustomUser(AbstractUser):
 
     def clean(self):
         from django.core.exceptions import ValidationError
- 
         if not self.id.isdigit():
             raise ValidationError("O campo da matrícula pode conter apenas números.")
 
     def __str__(self):
-        return self.id
+        return f"{self.first_name} {self.last_name} ({self.id})"
+
+
+class Card(models.Model):
+    user = models.OneToOneField(CustomUser, on_delete=models.CASCADE, related_name='card')
+    card_number = models.CharField(_("Número do Cartão"), max_length=20, unique=True)
+
+    def __str__(self):
+        return f'Cartão {self.card_number} - {self.user.first_name} {self.user.last_name}'
--- controllerapp/users/urls.py
+++ controllerapp/users/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('register/', views.register_view, name='register'),
     path('login/', views.login_view, name='login'),
     path('logout/', views.logout_view, name='logout'),
+    path('user-list/', views.user_list, name='user_list'),
 ]
