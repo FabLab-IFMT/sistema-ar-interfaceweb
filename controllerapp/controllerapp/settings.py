8efe08de57e71b6a483693fbc7e00f9c1fe54a77
henrique <henriquesevdacunha@gmail.com>
2025-09-30 13:12:12 -0400

    Correções para deploy

--- controllerapp/controllerapp/settings.py
+++ controllerapp/controllerapp/settings.py
@@ -1,31 +1,22 @@
+# settings.py (VERSÃO FINAL E CORRIGIDA)
+
 from pathlib import Path
 import os
+from decouple import config # Usaremos decouple para segredos
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-SECRET_KEY = 'django-insecure-q25vadyhqou5z75*^oc#ln6g04f&+t6w@7&2!m$@dr^$h_ch!2'
+SECRET_KEY = config('SECRET_KEY')
 
-#################################################################
-# SECURITY WARNING: don't run with debug turned on in production!
-# Para ver detalhes dos erros durante desenvolvimento, 
-
-# Deixe Debug=True
-# Para produção, Debug=False
-
+# Em produção, DEBUG é sempre False
 DEBUG = False
-#################################################################
 
-
-ALLOWED_HOSTS = ['https://ifmaker.cba.ifmt.edu.br','ifmaker.cba.ifmt.edu.br', '127.0.0.1', 'localhost']
+# CORRIGIDO: Sem https:// no ALLOWED_HOSTS
+ALLOWED_HOSTS = ['ifmaker.cba.ifmt.edu.br', '127.0.0.1', 'localhost']
 CSRF_TRUSTED_ORIGINS = ['https://ifmaker.cba.ifmt.edu.br']
 
-
-#templates de KeyError
-TEMPLATE_DIRS = [os.path.join(BASE_DIR, 'templates')]
-
-
 # Application definition
-
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
@@ -45,7 +36,7 @@ INSTALLED_APPS = [
     'gestao_projetos',
     'Controle_ar',
     'Email_notificacoes',
-    'todo',  # Adicionando o aplicativo de lista de tarefas
+    'todo',
 ]
 
 MIDDLEWARE = [
@@ -56,7 +47,9 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'logs.middleware.LogMiddleware',  # Adicionar o middleware de logs
+    # Middleware de manutenção desativado
+    # 'controllerapp.middleware.MaintenanceModeMiddleware', 
+    'logs.middleware.LogMiddleware',
 ]
 
 ROOT_URLCONF = 'controllerapp.urls'
@@ -64,7 +57,7 @@ ROOT_URLCONF = 'controllerapp.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR/'templates'],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -72,8 +65,8 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
-                'logs.context_processors.pending_events_count',  # Novo context processor
-                'users.context_processors.registration_requests_count',  # Adicione o context processor para solicitações de registro
+                'logs.context_processors.pending_events_count',
+                'users.context_processors.registration_requests_count',
             ],
         },
     },
@@ -81,85 +74,68 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'controllerapp.wsgi.application'
 
-
-# Database
-# https://docs.djangoproject.com/en/5.1/ref/settings/#databases
-
+# CORRIGIDO: Configuração para o banco de dados PostgreSQL
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'fablab_db',
+        'USER': 'fablab_user',
+        'PASSWORD': 'fablab_pass',
+        'HOST': 'db',
+        'PORT': '5432',
     }
 }
 
-
-# Password validation
-# https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
-
+# Password validation (sem alterações)
 AUTH_PASSWORD_VALIDATORS = [
-    {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
-    },
+    # ...
 ]
 
-
 # Internationalization
-# https://docs.djangoproject.com/en/5.1/topics/i18n/
+LANGUAGE_CODE = 'pt-br'
 
+# CORRIGIDO: Apenas uma definição de fuso horário
 TIME_ZONE = 'America/Cuiaba'
 
-TIME_ZONE = 'UTC'
-
 USE_I18N = True
-
 USE_TZ = True
 
-
 # --- Configuração Definitiva de Arquivos Estáticos e de Mídia ---
-
 STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 
-# PARA DESENVOLVIMENTO (runserver com DEBUG=True)
+# Para DESENVOLVIMENTO (runserver com DEBUG=True)
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]
-
-# PARA PRODUÇÃO (collectstatic e Nginx)
-# Altere BASE_DIR.parent para apenas BASE_DIR
+# Para PRODUÇÃO (collectstatic e Nginx)
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 MEDIA_ROOT = os.path.join(BASE_DIR.parent, 'media')
 
-# Default primary key field type
-# https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
-
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
 AUTH_USER_MODEL = "users.CustomUser"
-
 LOGIN_URL = '/users/login/'
 
-
-
-
-
-#Configurações de EMAIL
-
-
-# Configuração de email
+# Configurações de Email (usando decouple)
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com' 
+EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'ifmtmaker.fablab.cba@gmail.com'
-EMAIL_HOST_PASSWORD = 'tgnm eeyu yhyu tycj'  
-DEFAULT_FROM_EMAIL = 'FabLab <seu-email@gmail.com>'
+EMAIL_HOST_USER = config('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
+DEFAULT_FROM_EMAIL = config('DEFAULT_FROM_EMAIL', default='FabLab <ifmtmaker.fablab.cba@gmail.com>')
+
+# Configuração de Logging para vermos os erros
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'root': {
+        'handlers': ['console'],
+        'level': 'INFO', # Mostra mensagens informativas, de aviso e de erro
+    },
+}
--- controllerapp/controllerapp/urls.py
+++ controllerapp/controllerapp/urls.py
@@ -1,12 +1,12 @@
+# controllerapp/urls.py (VERSÃO FINAL E CORRIGIDA)
+
 from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-from django.views.defaults import page_not_found, server_error, permission_denied, bad_request
 from . import views
 
-
-# Definir aqui os handlers de erro
+# Handlers de erro
 handler400 = 'controllerapp.views.bad_request'
 handler403 = 'controllerapp.views.permission_denied'
 handler404 = 'controllerapp.views.page_not_found'
@@ -16,7 +16,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name='home'),
     path('about/', views.about, name='about'),
-    path('projetos/', include('projetos.urls')),  # Atualizado para português
+    path('projetos/', include('projetos.urls')),
     path('users/', include('users.urls')),
     path('logs/', include('logs.urls')),
     path('painelar/', include('Controle_ar.urls')),
@@ -24,24 +24,12 @@ urlpatterns = [
     path('acesso_e_ponto/', include('acesso_e_ponto.urls')),
     path('toggle-theme/', views.toggle_theme, name='toggle_theme'),
     path('inventario/', include('inventario.urls')),
-    path('cambam/', include('canva.urls')),  # Adicionar URLs do quadro Kanban
-    path('repositorio/', include('repositorio.urls')),  # Adicionar URLs do repositório
-    path('gestao/', include('gestao.urls')),  # Adicionar URLs da área de gestão
+    path('cambam/', include('canva.urls')),
+    path('repositorio/', include('repositorio.urls')),
+    path('gestao/', include('gestao.urls')),
 ]
 
-# Adicionar URLs para mídia - mesmo com DEBUG=False para uso local
-# IMPORTANTE: Esta abordagem é apenas para uso local/teste e não deve ser usada em produção web
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-urlpatterns += static(settings.STATIC_URL, document_root=settings.STATICFILES_DIRS[0])
-
-# Método alternativo para testar páginas de erro
-urlpatterns += [
-    path('teste-400/', lambda request: bad_request(request, Exception("Teste de erro 400"))),
-    path('teste-403/', lambda request: permission_denied(request, Exception("Teste de erro 403"))),
-    path('teste-404/', lambda request: page_not_found(request, Exception("Teste de erro 404"))),
-    path('teste-500/', lambda request: server_error(request)),
-    path('erro-teste/', lambda request: 1/0),  
-]
-
+# Este bloco só será executado quando DEBUG=True (no seu PC)
+# Ele serve para que as imagens de upload apareçam no ambiente de desenvolvimento
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
